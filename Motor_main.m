aa455d7992822fb6ada6a92ef44109b451e18794
Joshua Anderson <joshua.wei.anderson@gmail.com>
2024-02-01 12:48:40 -0600

    Added a square wave as a load and plotted.

--- Motor_main.m
+++ Motor_main.m
@@ -1,38 +1,45 @@
 clc;
-%Initial Conditions
-theta0 = 0;   %Initial angle
-theta_dot0 = 0;  %Initial angular velocity
+% Initial Conditions
+theta0 = 0;   % Initial angle
+theta_dot0 = 0;  % Initial angular velocity
 x0 = [theta0; theta_dot0];
 
-%Simulation Parameters
-T = 0.2;         %Simulation time
+% Simulation Parameters
+T = 0.2;         
 dt = 0.001;
-%Square wave config
-square_t = 0:dt:T;
-square_x = square(square_t);
 
-%Controller Params
+% Controller Params
 tau_values = [1 2 3 4];
-load_values = -5;
+%load_values = -5;
+
+%square config
+sq_amplitude = 200;
+sq_period = 2*pi*10;
+square_t = 0:dt:T;
+square_x = square_wave(square_t, sq_amplitude, sq_period);
 
 % Prepare the figure
 figure;
-hold on;
-grid on;
-colors = ['b', 'r', 'g', 'w']; % Define a set of colors
+colors = ['b', 'r', 'g', 'm'];
 
-%Using MATLAB ode45's Runge-Kutta integration:
+% Plot the square wave before the loop
+subplot(2, 1, 1);
+plot(square_t, square_wave(square_t,0.5,sq_period), '.-','HandleVisibility','off'); % Plot the square wave
+%hold on;
+
+% Using MATLAB ode45's Runge-Kutta integration:
 for i = 1:length(tau_values)
+    
+    [time, x] = ode45(@(t,x) Motor(t, x, square_wave(t,sq_amplitude,sq_period), tau_values(i)), [0 T], x0);
 
-    [time, x] = ode45(@(t,x) Motor(t, x, load_values, tau_values(i)), [0 T], x0);
     % Plot original solution
-    subplot(2, 1, 1); % Two rows, one column, first plot
+    subplot(2, 1, 1);
     hold on;
     plot(time, x(:,1), colors(i));
     grid on;
     xlabel('Time [s]');
     ylabel('Shaft Velocity [rad/s]');
-    title(['Shaft Velocity Responses for Different Tau Values (Load = ' num2str(load_values) 'kg)']);
+    title(['Shaft Velocity Responses for Different Tau Values (Load = ' num2str(sq_amplitude) '*square(' num2str(sq_period) '*t) kg)']);
     
     % Compute and plot derivative
     subplot(2, 1, 2); % Two rows, one column, second plot
@@ -42,10 +49,9 @@ for i = 1:length(tau_values)
     xlabel('Time [s]');
     ylabel('Shaft Acceleration [rad/s^2]');
     title('Shaft Acceleration');
-
 end
 
-% Add legends
+% Add legends for the response curves only
 subplot(2, 1, 1);
 legend(arrayfun(@(tau) ['tau = ', num2str(tau)], tau_values, 'UniformOutput', false), 'Location', 'best');
 subplot(2, 1, 2);
@@ -54,3 +60,7 @@ sgtitle('$T+Fr = \ddot{\theta_1}(I_r+\frac{I_g}{N}) + k_t\dot{\theta}$','Interpr
 
 hold off;
 
+% Square wave config
+function square_out = square_wave(t, amplitude, period)
+    square_out = amplitude*square(period*t);
+end
